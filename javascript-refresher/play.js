f09b37e11aa0eb4d610a1542ec41af9a24ed769e
BramMN <Brammerten@live.nl>
2022-12-20 12:05:33 +0100

    arrow functions

--- javascript-refresher/play.js
+++ javascript-refresher/play.js
@@ -1,11 +1,21 @@
-const name = "Bram"
+const userName = "Bram"
 let age = 25
 const hasHobbies = true
 
 age = 26
 
-function summarizeUser(userName, userAge, userHasHobby) {
+const summarizeUser = (userName, userAge, userHasHobby) => {
   return "Name is " + userName + ", age is " + userAge + " and the user has hobbies: " + userHasHobby
 }
 
-console.log(summarizeUser(name, age, hasHobbies))
+//function summarizeUser(userName, userAge, userHasHobby) {}
+
+// const add = (a, b) => a + b
+// const addOne = a => a + 1
+const addRandom = () => 1 + 2
+
+//console.log(add(1, 2))
+//console.log(addOne(1))
+console.log(addRandom())
+
+console.log(summarizeUser(userName, age, hasHobbies))
