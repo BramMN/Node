702a025cd7a5edc6f52a7e607edb2a6f04db0e21
BramMN <Brammerten@live.nl>
2022-12-20 12:08:18 +0100

    objects, properties and methods

--- javascript-refresher/play.js
+++ javascript-refresher/play.js
@@ -1,21 +1,9 @@
-const userName = "Bram"
-let age = 25
-const hasHobbies = true
-
-age = 26
-
-const summarizeUser = (userName, userAge, userHasHobby) => {
-  return "Name is " + userName + ", age is " + userAge + " and the user has hobbies: " + userHasHobby
+const person = {
+  name: "Bram",
+  age: 25,
+  greet() {
+    console.log("Hi, I am " + this.name)
+  },
 }
 
-//function summarizeUser(userName, userAge, userHasHobby) {}
-
-// const add = (a, b) => a + b
-// const addOne = a => a + 1
-const addRandom = () => 1 + 2
-
-//console.log(add(1, 2))
-//console.log(addOne(1))
-console.log(addRandom())
-
-console.log(summarizeUser(userName, age, hasHobbies))
+person.greet()
