066afd5d9c70f4e72e6640e1307f94c1b712ba74
BramMN <Brammerten@live.nl>
2022-12-21 11:11:46 +0100

    understanding generics

--- typescript/app.ts
+++ typescript/app.ts
@@ -2,7 +2,7 @@ const num1Element = document.getElementById("num1")! as HTMLInputElement
 const num2Element = document.getElementById("num2")! as HTMLInputElement
 const buttonElement = document.querySelector("button")!
 
-const numResults: number[] = []
+const numResults: Array<number> = []
 const textResults: string[] = []
 
 type NumOrString = number | string
@@ -35,4 +35,14 @@ buttonElement.addEventListener("click", () => {
   textResults.push(stringResult)
   console.log(numResults, textResults)
   printResult({ val: result, timestamp: new Date() })
+})
+
+const myPromise = new Promise<string>((resolve, reject) => {
+  setTimeout(() => {
+    resolve("It worked")
+  }, 1000)
+})
+
+myPromise.then(result => {
+  console.log(result.split("w"))
 })
