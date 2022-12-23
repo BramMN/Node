1f95b9750c5681137a891e1f2028daef06cd0867
BramMN <Brammerten@live.nl>
2022-12-23 11:11:44 +0100

    switching to named export

--- modern-js-&-node/response-handler.js
+++ modern-js-&-node/response-handler.js
@@ -1,10 +1,10 @@
 import fs from "fs"
 
-const resHandler = (req, res, next) => {
+export const resHandler = (req, res, next) => {
   fs.readFile("my-page.html", "utf8", (err, data) => {
     res.send(data)
   })
 }
 
 //module.exports = resHandler
-export default resHandler
+// export default resHandler
